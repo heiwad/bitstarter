9934af8daea7f95aa53fd709bd9a7feb0f82897a
Heiwad Osman <heiwad@server.fake>
2013-07-16 22:51:39 +0000

    Cleaned up Web.js and updated layout of index.html

--- web.js
+++ web.js
@@ -5,14 +5,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-  var buffer;
+  var indexBuffer;
   indexBuffer = fs.readFileSync("./index.html");
   response.send(indexBuffer.toString());
 
 });
 
-var port = process.env.PORT || 5000;
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
