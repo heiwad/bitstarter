03edf086afb121bafd8d67ccbcbb6268f94013c9
Heiwad Osman <heiwad@server.fake>
2013-07-23 09:41:52 +0000

    Updated with marketing copy

--- web.js
+++ web.js
@@ -11,6 +11,12 @@ app.get('/', function(request, response) {
 
 });
 
+app.get('/images/personalteleporter-logo.png', function (req, res) {
+    var indexBuffer;
+    indexBuffer= fs.readFileSync("./images/personalteleporter-logo.png");
+    res.send(indexBuffer);
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
